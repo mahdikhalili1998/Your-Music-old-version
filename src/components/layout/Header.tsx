bf00649991e11b5205867060759eb4b3913dc98e
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-08-11 19:47:26 +0330

    headeer edited

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,6 @@ import { FC, useEffect, useState } from "react";
 import { Iheader } from "@/types/header";
 import Link from "next/link";
 
-
 const Header: FC<Iheader> = ({ open, setOpen }) => {
   const clickHandler = () => {
     setOpen(true);
@@ -17,8 +16,7 @@ const Header: FC<Iheader> = ({ open, setOpen }) => {
       <div className="flex justify-between bg-gradient-to-r from-p-500 to-p-200 py-3 font-shantell">
         <h2 className="relative ml-2 mt-5 flex items-center p-2 text-3xl text-p-200">
           <Link href="/" className="flex items-center">
-            <span className="animate-moveDown mr-1 text-[2.5rem]"> Y </span>{" "}
-            your
+            <span className="animate-moveDown mr-1 text-[2.5rem]"> Y </span> our
             <span className="animate-TurnOff_on ml-2">Music</span>
           </Link>
           <span className="animate-moveDown mt-2">
@@ -33,7 +31,6 @@ const Header: FC<Iheader> = ({ open, setOpen }) => {
         </span>
       </div>
       <p className="half-circle -mt-1 h-6 w-full rotate-180 bg-gradient-to-r from-p-200 to-p-500"></p>
-  
     </div>
   );
 };
